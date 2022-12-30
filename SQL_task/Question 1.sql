777248c7667c8f0bb7e76f5a64caad2edc110b2f
ChanduGollavilli <61582494+chandugollavilli@users.noreply.github.com>
2022-12-30 15:47:59 +0530

    Add files via upload

---
+++ SQL_task/Question 1.sql
@@ -0,0 +1,37 @@
+
+-- CREATE DATABASE Employee;
+
+-- CREATE TABLE Employee_Details (  
+-- Emp_id INT NOT NULL PRIMARY KEY,
+-- Emp_name VARCHAR(50) NOT NULL,
+-- Job_name VARCHAR(50) NOT NULL,
+-- Manager_id INT NOT NULL,
+-- Hire_date DATE,
+-- Salary INT NOT NULL,
+-- Dep_id INT NOT NULL
+-- );
+
+-- SELECT * FROM Employee_Details;
+
+-- INSERT INTO Employee_Details (Emp_id,Emp_name,Job_name,Manager_id,Hire_date,Salary,Dep_id)
+-- VALUES (151,'Sherry Chadwick','CNC Operator',148811053,'2003-5-9',48810,1017),
+-- (131,'Leslie Bart','Cook',341571181,'2001-10-4',26489,1395),
+-- (188,'Norburn Ryan','Global Logistics Supervisor',761740207,'2009-7-19',43867,1612),
+-- (129,'Devonport Rhea','Inspector',814233225,'2011-10-3',22223,1654),
+-- (134,'Avery Makenzie','Global Logistics Supervisor',811612087,'2016-6-3',34457,1755),
+-- (119,'Wild Lorraine','Restaurant Manager',784261021,'2021-8-12',23282,1056),
+-- (198,'Hobson Kenzie','Business Broker',224731274,'2020-3-8',45623,1923),
+-- (165,'Hobson Karen','Banker',276742214,'2019-3-2',21527,1915),
+-- (144,'Matthews Marina','Call Center Representative',726247833,'2001-5-12',18201,1888),
+-- (155,'Knight Denny','Fabricator',712610668,'2008-11-5',20702,1082)
+
+-- SELECT Salary FROM employee_details;
+-- SELECT  sum(Salary) AS "Employee_Salary" FROM employee_details ;
+
+-- SELECT distinct Job_name From employee_details;
+
+
+
+
+
+
---
+++ SQL_task/Question 2.sql
@@ -0,0 +1,30 @@
+-- CREATE DATABASE Football;
+
+-- CREATE TABLE Football_Venue (  
+-- venue_id INT NOT NULL PRIMARY KEY,
+
+-- venue_name VARCHAR(50) NOT NULL,
+
+-- city_id INT NOT NULL,
+
+-- capacity INT NOT NULL
+-- );
+
+-- INSERT INTO Football_Venue(venue_id,venue_name,city_id,capacity)
+-- VALUES(20001,'France',4021,22431),
+-- (23456,'India',3392,12940),
+-- (22341,'Delhi',2959,19493),
+-- (12431,'Mumbai',3020,10493),
+-- (29530,'America',3959,1904),
+-- (20220,'SouthAfrica',2058,22941),
+-- (11958,'NorthAmerica',3958,22041),
+-- (38402,'Brazil',4392,23951),
+-- (20492,'Germany',2959,19431),
+-- (34930,'England',4202,49222)
+
+
+-- SELECT * FROM Football_Venue;
+
+-- SELECT count(venue_id) AS " football world cup Venues" FROM  Football_Venue;
+
+-- SELECT venue_name AS "Location",capacity AS "Volume" FROM Football_Venue;
